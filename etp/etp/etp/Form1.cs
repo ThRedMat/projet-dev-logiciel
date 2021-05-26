2552b22e970ba4c342fd7b99cd677b12f60ebf00
mathieu.darribau@ynov.com <mathieu.darribau@ynov.com>
2021-05-26 09:24:16 +0200

    etp

---
+++ etp/etp/etp/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace etp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        DataClasses1DataContext bd = new DataClasses1DataContext(); //accès a notre table 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //charger information à partir base donnée
+            var charger = (from a in bd.profil
+                select a.@ref + "---" + a.pseudo + "---" + a.score + "---");
+            // copier dans une list box
+            listBox1.Items.AddRange(charger.ToArray());
+        }
+    }
+}
